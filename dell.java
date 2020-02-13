794ce2d2271d03fdc103611027ab16794560777d
jk24online <60465895+jk24online@users.noreply.github.com>
2020-02-13 11:24:11 +0530

    Update dell.java

--- dell.java
+++ dell.java
@@ -1,3 +1,6 @@
-This changed in remote repo
+Please follow slowley what iam doing in class  
+
+
+his changed in remote repo
 
 Hi this file is updating jk12online username and password for push th ecode in jk24online paytm project
